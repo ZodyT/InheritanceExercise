6762860e4a73e911bf1c139300c989fc2a6be807
ZodyT <Zoraida1988@yahoo.com>
2020-11-25 11:25:58 -0600

    ReptileProperties

--- Inheritance/Reptile.cs
+++ Inheritance/Reptile.cs
@@ -14,5 +14,21 @@ namespace Inheritance
         public bool HasBackbone { get; set; }
         public bool IsPoisonous { get; set; }
         public bool LiveBearing { get; set; }
+
+        public Reptile()
+        {
+        }
+
+        public Reptile(bool isWarmBlooded, bool hasBackbone, bool isPoisonous, bool liveBearing, string name, string color, int age, int legs)
+        {
+            IsWarmBlooded = isWarmBlooded;
+            HasBackbone = hasBackbone;
+            IsPoisonous = isPoisonous;
+            LiveBearing = liveBearing;
+            Name = name;
+            Age = age;
+            Color = color;
+            Legs = legs;
+        }
     }
 }
